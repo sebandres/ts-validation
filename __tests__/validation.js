e771050b07e837eea12929a8d680a7c486b7b7c8
sebandres <sebandres@gmail.com>
2016-11-01 13:23:06 +1100

    initial failing test setup

---
+++ __tests__/validation.js
@@ -0,0 +1,10 @@
+var User = (function () {
+    function User() {
+    }
+    return User;
+}());
+describe('Validation', function () {
+    it('Should return an error item when object is not passed in', function () {
+        expect(false).toBe(true);
+    });
+});
